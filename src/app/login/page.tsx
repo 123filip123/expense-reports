b018ce2ea1b483510005278e1668365f8afbd296
Filip Popovski <filippopovski1998@gmail.com>
2025-01-18 14:11:43 +0100

    redirect logged in user to home if on login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,17 @@ import { login } from "./actions";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { PageWrapper } from "@/components/ui/page-wrapper/page-wrapper";
+import { createClient } from "@/libs/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (data?.user && !error) {
+    redirect("/");
+  }
+
   return (
     <PageWrapper>
       <div className="flex flex-col items-center justify-center h-full my-20">
